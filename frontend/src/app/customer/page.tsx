d7eda38e6a79219c0174b58afcbd8f077204d44a
Dhanuka Navodya <162307420+DhanukaNavodya@users.noreply.github.com>
2025-04-27 13:35:52 +0530

    feat:fe(offer):display offers

--- frontend/src/app/customer/page.tsx
+++ frontend/src/app/customer/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import DisplayCategories from "@/components/common/categories/categories"; // Adjust the import path as needed
 import RestaurantsSlider from "@/components/common/restaurants/restaurantSlider";
+import MenuItemsWithOffers from "@/components/customer/offer-menu-items";
 function Page() {
   return (
     <div>
+      <MenuItemsWithOffers/>
       <DisplayCategories />
       <RestaurantsSlider/>
     </div>
